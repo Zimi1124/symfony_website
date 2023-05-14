d98e8274de1da65d919ff1c77b263102739c28ea
Szymon <zimi1124@gmail.com>
2023-05-15 00:31:50 +0200

    Basic pages configuration with db communication

---
+++ src/Controller/PricingController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PricingPlan;
+use App\Entity\PricingPlanFeature;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PricingController extends AbstractController
+{
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    #[Route('/pricing', name: 'pricing')]
+    public function index(): Response
+    {
+
+
+        $pricingPlans = $this->entityManager
+            ->getRepository(PricingPlan::class)
+            ->findAll();
+        $features = $this->entityManager
+            ->getRepository(PricingPlanFeature::class)
+            ->findAll();
+
+        return $this->render('pricing/index.html.twig', [
+            'pricing_plans' => $pricingPlans,
+            'features' => $features,
+        ]);
+    }
+}
